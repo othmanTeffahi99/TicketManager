351a5a30829fce4e6ff8c04119526835927e501e
Othmane Teffahi <othmandavis@gmail.com>
2024-10-15 14:50:13 +0100

    -ADD: Error Nuget and CLean up code

---
+++ TicketManager.Domain/Common/Errors/Error.Authentication.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketManager.Domain.Common.Errors
+{
+    public static partial class CommonErrors
+    {
+        public static class Authentication
+        {
+            public static Error InvalidCredentials => Error.Validation(
+                code: "Auth.InvalidCredentials",
+                description: "Invalid credentials.");
+        }
+    }
--- TicketManager.Infrastructure/DIContainerRegister.cs
+++ TicketManager.Infrastructure/DIContainerRegister.cs
@@ -3,32 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using TicketManager.Application.Common.Interfaces;
 using TicketManager.Domain.Repositories;
 using TicketManager.Infrastructure.Common.Persistence;
 using TicketManager.Infrastructure.Common.Repositories;
 using TicketManager.Infrastructure.Common.Security;
+using TicketManager.Infrastructure.Common.Security.TokenGenerator;
 using TicketManager.Infrastructure.Services;
 
 namespace TicketManager.Infrastructure
 {
     public static class DIContainerRegister
     {
-        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, ConfigurationManager configuration)
         {
             services.AddDbContext<AppDbContext>(opt =>
             {
                 opt.UseInMemoryDatabase("TicketManagerDb");
             });
 
+            services.AddAuth(configuration);
             services.AddScoped<IUnitOfWork, UnitOfWork>();
             services.AddScoped<ITicketRepository, ITicketRepository>();
             services.AddScoped<IUserRepository, IUserRepository>();
-            services.AddScoped<IAuthenticationService, AuthenticationService>();
-            services.AddScoped<IJwtTokenGenerator, IJwtTokenGenerator>();
             services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
+            services.AddScoped(typeof(IRepository<>), typeof(RepositoryBase<>));
+
+            return services;
+        }
+
+        public static IServiceCollection AddAuth(this IServiceCollection services,
+            ConfigurationManager configuration)
+        {
+            var jwtSettings = new JwtSettings();
+            configuration.Bind(JwtSettings.SETTINGS, jwtSettings);
+            services.AddSingleton(Options.Create(jwtSettings));
+            services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
+            services.AddAuthentication(defaultScheme: JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSettings.Issuer,
+                    ValidAudience = jwtSettings.Audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF32.GetBytes(jwtSettings.SecretKey))
+                });
+
 
             return services;
         }
